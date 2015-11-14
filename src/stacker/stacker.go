68430cb919074bc020d2b29b60a30873cee8ab96
Jingkai Tang <jingkaitang@gmail.com>
2015-11-14 13:47:54 +0800

    updated: stack

--- src/stacker/stacker.go
+++ src/stacker/stacker.go
@@ -21,4 +21,33 @@ func main() {
 		}
 		fmt.Println(item)
 	}
+
+	var ho stack.Stack
+	ho.Push("Xiaomi")
+	ho.Push("Huawei")
+	ho.Push(1999)
+	x, err := ho.Top()
+	fmt.Println(x)
+	ho.Push(-6e-4)
+	ho.Push("Apple")
+	ho.Push("MS")
+	x, err = ho.Top()
+	fmt.Println(x)
+	ho.Push(11.11)
+	fmt.Println("stack is empty?", ho.IsEmpty())
+	fmt.Printf("Len() == %d Cap == %d\n", ho.Len(), ho.Cap())
+	diff := ho.Cap() - ho.Len()
+	for i := 0; i < diff; i ++ {
+		ho.Push(strings.Repeat("*", diff - i))
+	}
+	fmt.Printf("Len() == %d  Cap() == %d\n", ho.Len(), ho.Cap())
+	for ho.Len() > 0 {
+		x, _ := ho.Pop()
+		fmt.Printf("%T %v\n", x, x)
+	}
+	fmt.Println("stack is empty?", ho.IsEmpty())
+	x, err = ho.Pop()
+	fmt.Println(x, err)
+	x, err = ho.Pop()
+	fmt.Println(x, err)
 }
